76cbeffcbe989e3281b762bf183821606a0bd487
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-09-13 18:50:38 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   README.md
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+
 fn main() {
-    // Original integer variable declaration
-    let x = 42;
-    println!("x: {}", x);
-    
-    // Variable reassignment (shadowing) within the same scope
-    let x = "forty-two";
-    println!("x: {}", x);
-    
-    {
-        // Creating a nested scope where 'x' has a new binding
-        let x = 42.5;
-        println!("x: {}", x);
-        
-        // Leaving the inner scope - original bindings restored
+
+
+    let name ="hello";
+
+    match name{
+        "hellov" =>{
+            print!("hello");
+        }
+        "hello" => print!("hdfbvi"),
+        "hi" => print!("hi"),
+
+        _ => print!("wrong"),
     }
+
 }
