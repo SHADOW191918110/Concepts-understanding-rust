c171150a10f96c093a1fffc87f513b4db211b227
SAIRAJ KHILARI <sairajkhilari191918110+github1@gmail.com>
2025-09-26 17:22:31 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   README.md
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
+// fn get_input_user() -> String {
+//     let mut input = String::new();
+//     std::io::stdin()
+//         .read_line(&mut input)
+//         .expect("failed to read input");
+//     input.trim().to_string()
+// }
+// use std::fmt::Debug;
 
-fn main() {
+// fn helo<T: Debug>(x: T) {
+//     println!("hello, {:?}", x);
+// }
+
+// fn main() {
+//     let x = "00sdvfsgs";
+//     helo(x);
+// }
+// let  x = 4;
+
+// let aa = if x >= 5 {
+//     Ok(x)
+// } else {
+//     Err("Number is less than five!".to_string())
+// };
+
+// println!("{:?}",aa);
+// }
+
+use std::collections::{hash_map, HashMap};
 
 
-    let name ="hello";
+fn main(){
+//     let mut hashmappp = HashMap :: new();
+//     hashmappp.insert ("sairaj" ,655);
+//     hashmappp.insert ("soham" ,677);
+//     hashmappp.insert ( "ankit" , 688);
 
-    match name{
-        "hellov" =>{
-            print!("hello");
-        }
-        "hello" => print!("hdfbvi"),
-        "hi" => print!("hi"),
 
-        _ => print!("wrong"),
-    }
+//     println!("{:?}",hashmappp); 
+
+    let mut var1 = "shadow";
+
+    let get = &var1[0..5];
+    print!("{:?}",get) ;
+
+
 
 }
+
+
